ca6d22b18937640b75aaa1d670e7f2d065c2a574
phatdroid94 <phatdroid94@gmail.com>
2024-08-06 18:23:15 +0700

    setup widget

--- lib/demo_context_page.dart
+++ lib/demo_context_page.dart
@@ -14,7 +14,50 @@ class _DemoContextPageState extends State<DemoContextPage> {
       appBar: AppBar(
         title: Text("Demo context page"),
       ),
-      body: Container(),
+      body: Container(
+        child: Center(
+          child: ParentWidget(
+            child: ChildWidget(),
+          ),
+        ),
+      ),
     );
   }
 }
+
+
+class ParentWidget extends StatefulWidget {
+  Widget child;
+
+  ParentWidget({required this.child});
+
+  @override
+  State<ParentWidget> createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text("Parent widget"),
+          widget.child
+        ],
+      ),
+    );
+  }
+}
+
+class ChildWidget extends StatelessWidget {
+  const ChildWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Child widget"),
+    );
+  }
+}
+
+
